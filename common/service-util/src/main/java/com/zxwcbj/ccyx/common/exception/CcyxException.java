498d33741294b9c10ddfb5d96f6e546dc6fd8da9
onsimple <a848067526@163.com>
2023-11-01 11:08:57 +0800

    创建全局异常处理类(完成)

---
+++ common/service-util/src/main/java/com/zxwcbj/ccyx/common/exception/CcyxException.java
@@ -0,0 +1,39 @@
+package com.zxwcbj.ccyx.common.exception;
+
+import com.zxwcbj.ccyx.common.result.ResultCodeEnum;
+
+/**
+ * 统一异常处理
+ * @author a8480
+ */
+public class CcyxException extends Throwable {
+    private Integer code;
+    private String message;
+
+    /**
+     * 使用指定的详细信息消息构造一个新的运行时异常;
+     *  @param code;
+     * @param message;
+     */
+    public  CcyxException(Integer code,String message){
+        /*
+        * 之所以使用super(message)是因为父类中有一个message属性
+        * 如果不使用super(message)来调用父类的message属性
+        * 那么调用父类的message属性时,就会产生空指针异常
+        * */
+        super(message);
+        this.code = code;
+    }
+    public CcyxException(ResultCodeEnum resultCode){
+        super(resultCode.getMessage());
+        this.code = resultCode.getCode();
+    }
+
+    @Override
+    public String toString() {
+        return "CcyxException{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
--- common/service-util/src/main/java/com/zxwcbj/ccyx/common/exception/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/zxwcbj/ccyx/common/exception/GlobalExceptionHandler.java
@@ -4,24 +4,40 @@ import com.zxwcbj.ccyx.common.result.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * 全局异常处理类
  *
- **/
-
+ * @author a8480
+ */
+/**
+ * 是 Spring MVC 框架中的一个关键组件，负责将请求映射到具体的方法并处理这些方法的执行
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
     /**
      * 异常处理器.当你出现这个异常时调用下面的方法
-     * */
+     */
     @ExceptionHandler(Exception.class)
+
     /**
      * 返回json数据
-     */
+     * */
     @ResponseBody
-    public Result error(Exception e){
+    public Result error(Exception e) {
         e.printStackTrace();
-         return Result.fail(null);
+        return Result.fail(null);
     }
 
+    /**
+     * 自定义异常处理方法
+     *
+     * @param ccxException
+     * @return
+     */
+    @ExceptionHandler(CcyxException.class)
+    @ResponseBody
+    private Result error(CcyxException ccxException) {
+        return Result.fail(null);
+    }
 }
